c57da07db43733bb7d6c4d5b2211e9b104ed389f
dariohirsch <darohirsch@gmail.com>
2021-09-29 10:21:24 +0200

    asda

--- app.js
+++ app.js
@@ -1,33 +1,30 @@
-require("dotenv/config");
-require("./db");
-const express = require("express");
+require("dotenv/config")
+require("./db/index.js")
+const express = require("express")
 
-const { isAuthenticated } = require("./middleware/jwt.middleware"); // <== IMPORT
+const { isAuthenticated } = require("./middleware/jwt.middleware") // <== IMPORT
 
-
-const app = express();
-require("./config")(app);
-
+const app = express()
+require("./config")(app)
 
 // 👇 MIDDLEWARE MISSING
-const allRoutes = require("./routes");
-app.use("/api", allRoutes);
+const allRoutes = require("./routes")
+app.use("/api", allRoutes)
 
-const authRouter = require("./routes/auth.routes");
-app.use("/api/auth", authRouter);
+const authRouter = require("./routes/auth.routes")
+app.use("/api/auth", authRouter)
 
-const projectRouter = require("./routes/project.routes");
-app.use("/api", isAuthenticated, projectRouter);            // <== UPDATE
+const projectRouter = require("./routes/project.routes")
+app.use("/api", isAuthenticated, projectRouter) // <== UPDATE
 
-const taskRouter = require("./routes/task.routes");
-app.use("/api",isAuthenticated, taskRouter);            // <== UPDATE
-
+const taskRouter = require("./routes/task.routes")
+app.use("/api", isAuthenticated, taskRouter) // <== UPDATE
 
 app.use((req, res, next) => {
-    // If no routes match, send them the React HTML.
-    res.sendFile(__dirname + "/public/index.html");
-  });
+	// If no routes match, send them the React HTML.
+	res.sendFile(__dirname + "/public/index.html")
+})
 
 // require("./error-handling")(app);
 
-module.exports = app;
+module.exports = app
