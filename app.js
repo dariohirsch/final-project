8930a303d1b31083583cef09a708fc0b6ff37dae
dariohirsch <darohirsch@gmail.com>
2021-09-29 10:40:45 +0200

    prueba

--- app.js
+++ app.js
@@ -8,11 +8,12 @@ const app = express()
 require("./config")(app)
 
 // 👇 MIDDLEWARE MISSING
-const allRoutes = require("./routes")
-app.use("/api", allRoutes)
 
 const authRouter = require("./routes/auth.routes")
 app.use("/api/auth", authRouter)
+
+const allRoutes = require("./routes")
+app.use("/api", allRoutes)
 
 const projectRouter = require("./routes/project.routes")
 app.use("/api", isAuthenticated, projectRouter) // <== UPDATE
@@ -20,11 +21,11 @@ app.use("/api", isAuthenticated, projectRouter) // <== UPDATE
 const taskRouter = require("./routes/task.routes")
 app.use("/api", isAuthenticated, taskRouter) // <== UPDATE
 
-app.use((req, res, next) => {
-	// If no routes match, send them the React HTML.
-	res.sendFile(__dirname + "/public/index.html")
-})
+// app.use((req, res, next) => {
+// 	// If no routes match, send them the React HTML.
+// 	res.sendFile(__dirname + "/public/index.html")
+// })
 
-// require("./error-handling")(app);
+require("./error-handling")(app)
 
 module.exports = app
